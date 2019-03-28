121020424832428e9ee39b8fecca302bf22a9bab
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-03-28 20:36:54 +0200

    Delete orders.py

--- src/orders/orders.py
+++
@@ -1,4 +0,0 @@
-import local_settings as ls
-class Orders():
-    def get_order(bot,update, user_data):
-        bot.send_message(text = 'Хорошо, введите название выбранной книги', chat_id = update.message.chat_id, message_id = update.message.message_id)
