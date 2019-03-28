0e11533b80532f27c82ebeb26f97ff3b0b737a6a
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-03-28 10:53:47 +0200

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from send_to_users import send_messages
 from local_settings import Token
 from orders.orders import  get_order
 from dbscripts.add_book import  get_new_books
@@ -7,12 +8,13 @@ from menus.menus import start, keyboard_functions, get_adm
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 updater = Updater(Token)
 dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("start", start,pass_user_data=True))
 dp.add_handler(CommandHandler("admin", get_adm, pass_user_data=True))
 dp.add_handler(CommandHandler("order", get_order))
 dp.add_handler(CommandHandler("add", get_new_books, pass_user_data=True))
-dp.add_handler(CommandHandler("change",new_description, pass_user_data= True))
+dp.add_handler(CommandHandler("change", new_description, pass_user_data= True))
 dp.add_handler(CommandHandler("delete", delete_book, pass_user_data= True))
+dp.add_handler(CommandHandler("sendall", send_messages))
 dp.add_handler(CallbackQueryHandler(keyboard_functions, pass_user_data=True))
 MessageHandler(Filters.text, get_new_books, pass_user_data=True)
 MessageHandler(Filters.text, new_description, pass_user_data= True)
--- src/models.py
+++ src/models.py
@@ -1,18 +1,38 @@
 from local_settings import my_db
 from sqlalchemy import create_engine
-from  sqlalchemy import Column, String
-from  sqlalchemy.orm import sessionmaker
-from  sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 db = create_engine(my_db)
 base = declarative_base()
+
+
 class Books(base):
     __tablename__ = 'products'
-
-    title = Column(String,primary_key=True)
+    """
+    id_book = Column(Integer, primary_key = True)
+    """
+    title = Column(String, primary_key=True)
     description = Column(String)
-    price = Column(String)
+    price = Column(String) #Тоже должно быть integer
+    """
+    examples = Column(Integer)
+    """
 
+class Users(base):
+    __tablename__ = 'botusers'
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String)
+"""
+
+class Orders(base):
+    __tablename__ = 'orders'
+    order_id = Column(Integer, primary_key=True)
+    order_book = Column(String) 
+    customer_email = Column(String)
+    customer_phone = Column(Integer)
+    
+"""
 Session = sessionmaker(db)
 session = Session()
 base.metadata.create_all(db)
-books = session.query(Books)
