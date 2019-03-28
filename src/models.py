41a04b83d47e3e6f18cb8f9b25aad9472cae6b1a
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-03-28 10:57:58 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -9,30 +9,17 @@ base = declarative_base()
 
 class Books(base):
     __tablename__ = 'products'
-    """
-    id_book = Column(Integer, primary_key = True)
-    """
+    
     title = Column(String, primary_key=True)
     description = Column(String)
-    price = Column(String) #Тоже должно быть integer
-    """
-    examples = Column(Integer)
-    """
+    price = Column(String) 
+
 
 class Users(base):
     __tablename__ = 'botusers'
     user_id = Column(Integer, primary_key=True)
     name = Column(String)
-"""
 
-class Orders(base):
-    __tablename__ = 'orders'
-    order_id = Column(Integer, primary_key=True)
-    order_book = Column(String) 
-    customer_email = Column(String)
-    customer_phone = Column(Integer)
-    
-"""
 Session = sessionmaker(db)
 session = Session()
 base.metadata.create_all(db)
