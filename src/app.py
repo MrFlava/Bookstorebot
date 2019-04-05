04c701543513a08bc1f3df5bf325c77a87259bf3
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-04-05 13:47:03 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from local_settings import Token
-from menus.menus import menus_handler
+from dbscripts.delete_book import delete_handler
+from menus.menus import menus_handler, admkeyboard_handler
 from telegram.ext import Updater
 def main():
     updater = Updater(Token)
     dp = updater.dispatcher
     dp.add_handler(menus_handler)
+     dp.add_handler(admkeyboard_handler)
+    dp.add_handler(delete_handler)
     updater.start_polling()
     updater.idle()
 
