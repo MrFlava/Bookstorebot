c936c0a5b4c7cd4432c5b8a6f60396f6d601ecca
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-04-08 10:31:25 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from local_settings import Token
+from send_to_users import sends_handler
 from dbscripts.add_book import record_handler
 from dbscripts.orders import exist_handler, notexist_handler
 from dbscripts.delete_book import delete_handler
