9c53572a146111fed9411ae5e55473b440317367
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-04-08 23:38:02 +0300

    Update menus.py

--- src/menus/menus.py
+++ src/menus/menus.py
@@ -1,14 +1,7 @@
-import os
 import local_settings as ls
-"""
-from  botmanlib.menus.basemenu import  BaseMenu 
- вызов basmenu ( дойду до этого, как только бот без этой библиотеки будет нормальным)
-"""
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import CommandHandler, CallbackQueryHandler, ConversationHandler
-datapath = os.path.dirname(__file__)
-models = os.path.join(datapath, "bookstorebot/src/models.py")
-from models import Users, Products, session
+from src.models import Users, Products, session
 KEYBOARD, ADMKEYBOARD = range(2)
 
 def start(bot, update,user_data):
