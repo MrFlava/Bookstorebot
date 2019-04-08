4450fd50fc457769b7fe9fa9060fe69d99de2ef8
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-04-08 10:30:27 +0300

    Delete send_to_users.py

--- src/send_to_users.py
+++
@@ -1,8 +0,0 @@
-from  models import Users,session
-def send_messages(bot,update):
-    users = session.query(Users)
-    for user in users:
-        user  = user.user_id
-        bot.send_message(chat_id=user, text="Хэй, хэй, хээй, завезли новые книги!")
-    bot.send_message(text='Рассылка произведена!', chat_id=update.message.chat_id,
-                         message_id=update.message.message_id)
