0c5fe0b0aada6fd11e0e7d3e8a17777114d78f60
MrFlava <44810093+MrFlava@users.noreply.github.com>
2019-04-16 10:49:23 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,11 +2,11 @@ import os
 import logging
 from src.settings import MEDIA_FOLDER, RESOURCES_FOLDER
 from botmanlib.bot import BotmanBot
-from src.dbscripts.send_to_users import sends_handler
-from src.dbscripts.add_book import record_handler
-from src.dbscripts.orders import exist_handler, notexist_handler
-from src.dbscripts.change_desc import desc_handler
-from src.dbscripts.delete_book import delete_handler
+from src.scripts.send_to_users import sends_handler
+from src.scripts.add_book import record_handler
+from src.scripts.orders import exist_handler, notexist_handler
+from src.scripts.change_desc import desc_handler
+from src.scripts.delete_book import delete_handler
 from src.menus.startmenu import Menus
 from src.menus.admmenu import Admmenu
 from telegram.ext import Updater
