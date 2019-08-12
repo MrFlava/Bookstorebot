ec68d186950a44fc0b84292092f6c90430143e2a
MrFlava <thatelitemaili33t@gmail.com>
2019-08-13 01:10:14 +0300

    v1 update

--- src/app.py
+++ src/app.py
@@ -1,32 +1,78 @@
-import os
+import datetime
 import logging
-from src.settings import MEDIA_FOLDER, RESOURCES_FOLDER
+import os
+import pprint
+
 from botmanlib.bot import BotmanBot
-from src.scripts.send_to_users import sends_handler
-from src.scripts.add_book import record_handler
-from src.scripts.orders import exist_handler, notexist_handler
-from src.scripts.change_desc import desc_handler
-from src.scripts.delete_book import delete_handler
-from src.menus.startmenu import Menus
-from src.menus.admmenu import Admmenu
-from telegram.ext import Updater
+from botmanlib.updater import BotmanUpdater
+from telegram import Message, Chat, TelegramError
+from telegram.ext import MessageHandler, Filters, messagequeue, CallbackQueryHandler
+from telegram.utils.request import Request
+
+from src.menus.admin.admmenu import AdminMenu
+from src.menus.start.startmenu import StartMenu
+from src.models import User, DBSession
+from src.settings import MEDIA_FOLDER, RESOURCES_FOLDER, SETTINGS_FILE, WEBHOOK_PORT, WEBHOOK_IP, WEBHOOK_URL, WEBHOOK_ENABLE
+
+
 logger = logging.getLogger(__name__)
+
+
+def stop(update, context):
+    # if user never have a conversation with bot and sending /stop he has empty user_data
+    if context.user_data:
+        user = DBSession.query(User).filter(User.chat_id == context.user_data['user'].chat_id).first()
+        user.active = False
+        user.chat_room_id = None
+        DBSession.add(user)
+        DBSession.commit()
+        context.bot.send_message(chat_id=user.chat_id, text= ("Вы удалены из этого бота"))
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    pp = pprint.PrettyPrinter(indent=4)
+    logger.error(f'Update "{pp.pformat(str(update))}" caused error "{context.error}"')
+
+
+def goto_start(update, context):
+    if update.effective_message and update.effective_message.message_id != 0:
+        try:
+            context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
+        except TelegramError as e:
+            logger.error(str(e))
+
+    update.message = Message(0, update.effective_user, datetime.datetime.utcnow(), Chat(0, Chat.PRIVATE), text='/start', bot=context.bot)
+    update._effective_message = None
+    update.callback_query = None
+    context.update_queue.put(update)
+
+
 def main():
     bot_token = os.environ['bot.token']
-    bot = BotmanBot(token=bot_token)
-    updater = Updater(bot=bot)
-    dp = updater.dispatcher
-    start_menu = Menus(bot=bot, dispatcher=dp)
-    adm_menu = Admmenu(bot=bot, dispatcher=dp)
-    dp.add_handler(sends_handler)
-    dp.add_handler(start_menu.handler)
-    dp.add_handler(adm_menu.handler)
-    dp.add_handler(delete_handler)
-    dp.add_handler(exist_handler)
-    dp.add_handler(notexist_handler)
-    dp.add_handler(record_handler)
-    dp.add_handler(desc_handler)
+    mqueue = messagequeue.MessageQueue(all_burst_limit=30, all_time_limit_ms=1000)
+    request = Request(con_pool_size=8)
 
+    bot = BotmanBot(token=bot_token, request=request, mqueue=mqueue)
+    updater = BotmanUpdater(bot=bot, use_context=True)
+
+    dispatcher = updater.dispatcher
+    job_queue = updater.job_queue
+
+    # Handlers
+    start_menu = StartMenu(bot=bot, dispatcher=dispatcher)
+    admin_menu = AdminMenu(bot=bot, dispatcher=dispatcher)
+    stop_handler = MessageHandler(Filters.regex('^/stop$'), stop)
+
+    # adding menus
+    dispatcher.add_handler(stop_handler)
+    dispatcher.add_handler(admin_menu.handler)
+    dispatcher.add_handler(start_menu.handler)
+    dispatcher.add_handler(MessageHandler(Filters.all, goto_start))
+    dispatcher.add_handler(CallbackQueryHandler(goto_start))
+    dispatcher.add_error_handler(error)
+
+    # Start bot
     if not os.path.exists(MEDIA_FOLDER):
         os.mkdir(MEDIA_FOLDER)
         logger.info("Media folder created")
@@ -34,10 +80,25 @@ def main():
         os.mkdir(RESOURCES_FOLDER)
         logger.info("Resources folder created")
 
-    logger.info("Bot started")
+    if not os.path.exists(SETTINGS_FILE):
+        with open(SETTINGS_FILE + ".default", 'r') as settings:
+            out = open(SETTINGS_FILE, 'w')
+            out.write(settings.read())
+            out.close()
+        logger.info("Settings file created")
 
-    updater.start_polling()
+
+    if WEBHOOK_ENABLE:
+        update_queue = updater.start_webhook(listen=WEBHOOK_IP, port=WEBHOOK_PORT, allowed_updates=['message', 'edited_message', 'callback_query'])
+        updater.bot.set_webhook(url=WEBHOOK_URL)
+    else:
+        update_queue = updater.start_polling(allowed_updates=['message', 'edited_message', 'callback_query'])
+
+    logger.info("Bot started")
     updater.idle()
+
+    bot.stop()
+
 
 if __name__ == "__main__":
     main()
--- src/models.py
+++ src/models.py
@@ -1,40 +1,44 @@
-import os
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import sessionmaker
-if not os.getenv('sqlalchemy.url'):
-    from src.settings import PROJECT_ROOT
-    import configparser
-    config = configparser.ConfigParser()
-    config.read(os.path.join(PROJECT_ROOT, 'botmanlib.ini'))
-    os.environ['sqlalchemy.url'] = config['botmanlib']['sqlalchemy.url']
-from botmanlib.models import db
-base = db.Base
-db = create_engine(db.database_url)
-class Products(base):
-    __tablename__ = 'sell'
-    __table_args__ = {'extend_existing': True}
-    id_book = Column(Integer, primary_key=True)
-    prod_name = Column(String)
-    about = Column(String)
-    cost = Column(Integer)
-    edition = Column(Integer)
+from botmanlib.models import DB, User as BUser, Permission as BPermission, users_permissions_table
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import relationship
 
-class Users(base):
-    __tablename__ = 'botusers'
-    __table_args__ = {'extend_existing': True}
-    user_id = Column(Integer, primary_key=True)
-    name = Column(String)
+database = DB()
+Base = database.Base
 
-class Orderbooks(base):
-    __tablename__ = 'orderedbooks'
-    __table_args__ = {'extend_existing': True}
-    nickname = Column(String, primary_key=True)
-    bookname = Column(String)
-    email = Column(String)
-    phone = Column(Integer)
 
-Session = sessionmaker(db)
-session = Session()
-base.metadata.create_all(db)
+class User(BUser, Base):
+    __tablename__ = 'users'
+    permissions_association_table = users_permissions_table(Base.metadata)
+    came_from = Column(String)
+    has_order = Column(Boolean, default=False)
+
+    def init_permissions(self):
+        for permission in ['start_menu_access',
+                           'profile_menu_access',
+                           'change_language_menu_access',
+                           'marketplace_menu_access',
+                           'price_analysis_menu_access',
+                           'make_query_menu_access',
+                           'queries_history_menu_access',
+                           'buy_queries_menu_access',
+                           'operators_menu_access']:
+            perm = DBSession.query(Permission).get(permission)
+            if perm not in self.permissions:
+                self.permissions.append(perm)
+
+
+class Permission(BPermission, Base):
+    __tablename__ = 'permissions'
+
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    genre = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    image = Column(String)
+    price = Column(Integer)
+
+DBSession = database.DBSession
+Base.metadata.create_all(database.engine)
